6642bcb5d1225aec58c319c9483bf370a37dd8c7
Ricardo Lima <ricardoccofg@gmail.com>
2018-11-07 23:57:54 -0200

    subindo consulta de vagas e consulta de profissao

--- src/main/java/br/com/api/repository/UsuarioRepository.java
+++ src/main/java/br/com/api/repository/UsuarioRepository.java
@@ -1,15 +1,12 @@
 package br.com.api.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import br.com.api.model.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
-	@Query("SELECT NEW Usuario(u.id, u.nome) FROM Usuario u JOIN Vaga v ON  WHERE ") //FAZER COM QUE RETORNE UMA LISTA DE USUARIOS POR ID DA VAGA.
-	List<Usuario> listarUsuariosParticipandoDaVaga(Long idVaga);
+	/*@Query("SELECT NEW Usuario(u.id, u.nome) FROM Usuario u JOIN Vaga v ON  WHERE ") //FAZER COM QUE RETORNE UMA LISTA DE USUARIOS POR ID DA VAGA.
+	List<Usuario> listarUsuariosParticipandoDaVaga(Long idVaga);*/
 
 }
