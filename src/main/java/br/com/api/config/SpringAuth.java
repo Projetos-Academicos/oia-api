29aa49e321614a30f455ee5dfbc301185de43604
Ricardo Lima <ricardoccofg@gmail.com>
2018-11-10 02:44:30 -0200

    implementando o login

---
+++ src/main/java/br/com/api/config/SpringAuth.java
@@ -0,0 +1,51 @@
+package br.com.api.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+@Configuration
+@EnableAuthorizationServer
+public class SpringAuth extends AuthorizationServerConfigurerAdapter {
+
+	@Autowired
+	private AuthenticationManager authMenager;
+
+	@Bean
+	public JwtAccessTokenConverter accessTokenConverter() {
+		JwtAccessTokenConverter accessTokenConverter = new JwtAccessTokenConverter();
+		accessTokenConverter.setSigningKey("teste");
+		return accessTokenConverter;
+	}
+
+	@Override
+	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+		endpoints.tokenStore(this.tokenStore())
+		.accessTokenConverter(this.accessTokenConverter())
+		.authenticationManager(this.authMenager);
+	}
+
+	@Override
+	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+		clients.inMemory()
+		.withClient("teste")
+		.secret("teste123")
+		.scopes("read", "write")
+		.authorizedGrantTypes("password")
+		.accessTokenValiditySeconds(1800);
+	}
+
+	@Bean
+	public TokenStore tokenStore() {
+		return new JwtTokenStore(this.accessTokenConverter());
+	}
+}
+
